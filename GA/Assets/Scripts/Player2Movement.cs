46ae28770934463fb56cbad2639b471f612c6b7e
noznibla <faal00001@utb.vaxjo.se>
2019-03-15 18:36:32 +0100

    First commit
    La till Unity Project till GitHub

---
+++ GA/Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    bool fire1 = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal2") * runSpeed;
+
+        /* animator.SetFloat("Speed", Mathf.Abs(horizontalMove)); */
+
+        if (Input.GetButtonDown("Jump2"))
+        {
+            jump = true;
+           /* animator.SetBool("IsJumping", true); */
+        }
+
+        if (Input.GetButtonDown("Crouch2"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch2"))
+        {
+            crouch = false;
+        }
+
+
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
